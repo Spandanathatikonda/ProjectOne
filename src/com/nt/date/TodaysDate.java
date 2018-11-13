ec570a01f9f74666c2c43400ecc35905855b29f4
Hema Latha KB <Hema Latha KB@HemaLatha>
2018-11-13 11:43:39 +0530

    Modified By Snehalatha

--- src/com/nt/date/TodaysDate.java
+++ src/com/nt/date/TodaysDate.java
@@ -4,7 +4,8 @@ public class TodaysDate {
 
 	public static void main(String[] args) {
 		System.out.println(new java.util.Date().getTime());
-
+		System.out.println("This is from snehalatha modified");
 	}
 
+	
 }
